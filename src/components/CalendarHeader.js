2fdad047fd7f90a193c78afbb69d67a89b96cb4c
Samyak2605 <samyak.m23csai@nst.rishihood.edu.in>
2024-09-21 13:39:52 +0530

    done

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -2,14 +2,19 @@ import dayjs from "dayjs";
 import React, { useContext } from "react";
 import logo from "../assets/logo.png";
 import GlobalContext from "../context/GlobalContext";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 export default function CalendarHeader() {
   const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+
   function handlePrevMonth() {
     setMonthIndex(monthIndex - 1);
   }
+
   function handleNextMonth() {
     setMonthIndex(monthIndex + 1);
   }
+
   function handleReset() {
     setMonthIndex(
       monthIndex === dayjs().month()
@@ -17,33 +22,43 @@ export default function CalendarHeader() {
         : dayjs().month()
     );
   }
+
   return (
-    <header className="px-4 py-2 flex items-center">
-      <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
-      <h1 className="mr-10 text-xl text-gray-500 fond-bold">
-        Calendar
-      </h1>
-      <button
-        onClick={handleReset}
-        className="border rounded py-2 px-4 mr-5"
-      >
-        Today
-      </button>
-      <button onClick={handlePrevMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-          chevron_left
-        </span>
-      </button>
-      <button onClick={handleNextMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-          chevron_right
-        </span>
-      </button>
-      <h2 className="ml-4 text-xl text-gray-500 font-bold">
-        {dayjs(new Date(dayjs().year(), monthIndex)).format(
-          "MMMM YYYY"
-        )}
-      </h2>
+    <header className="px-4 py-2 flex items-center justify-between">
+      <div className="flex items-center">
+        <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
+        <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
+        <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
+          Today
+        </button>
+        <button onClick={handlePrevMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+        </button>
+        <button onClick={handleNextMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
+        <h2 className="ml-4 text-xl text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+        </h2>
+      </div>
+
+      <div>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="bg-blue-500 text-white py-2 px-4 rounded">
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </header>
   );
 }
